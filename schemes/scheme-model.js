110d2f92c23a5217e2a25ed7ca5897c3696e2371
Brandon Dong <gasingdong@gmail.com>
2019-09-11 13:25:06 -0700

    add findById and findSteps helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,6 +4,22 @@ const find = () => {
   return db('schemes');
 };
 
+const findById = id => {
+  return db('schemes')
+    .where({ id })
+    .first();
+};
+
+const findSteps = id => {
+  return db('steps as s')
+    .join('schemes as sc', 's.scheme_id', '=', 'sc.id')
+    .where({ scheme_id: id })
+    .select('s.id', 'sc.scheme_name', 's.step_number', 's.instructions')
+    .orderBy('step_number');
+};
+
 module.exports = {
   find,
+  findById,
+  findSteps,
 };
